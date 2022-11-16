fc9fb0c9df123737ce6c1928460b204dc643c8cf
ze2213 <kch971203@gmail.com>
2022-11-17 03:09:51 +0900

    메인페이지 대충 완성
    메인페이지 대충 완성

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-  $(".mobile-menu-btn a, .mobile-gnb .close-btn").on("click",function(){
+  $(".mobile-menu-btn a, .mobile-gnb .close-btn").on("click",function(){// 모바일 메뉴 버튼
     $(".mobile-gnb").toggleClass("on");
     return false
   })
 
-  var trailerswiper = new Swiper(".game-trailer .inner", {
+  $(".gnb-wrap .gnb li, .gnb-down-menu").on('mouseenter', function(){
+    $(".gnb-down-menu").fadeIn(2000)
+    console.log("on")
+  })
+  $(".gnb-wrap .gnb li, .gnb-down-menu").on('mouseleave', function(){
+    $(".gnb-down-menu").fadeOut(2000)
+    console.log("off")
+  })
+
+  var trailerswiper = new Swiper(".main-video-swiper", { //비디오 스와이퍼
     loop: true,
     pagination: {
       el: ".swiper-pagination",
@@ -15,7 +24,7 @@ $(document).ready(function(){
     },
   });
 
-  var fractionSwiper = new Swiper(".game-trailer .inner", {
+  var fractionSwiper = new Swiper(".main-video-swiper", { //비디오 스와이퍼(pagination 하나 더 추가)
     pagination: {
       el: ".swiper-pagination2",
       type: "fraction",
@@ -23,7 +32,13 @@ $(document).ready(function(){
   });
   trailerswiper.controller.control = fractionSwiper;
 
-
+  var setion2Swiper = new Swiper(".section2-swiper", { // section2 스와이퍼
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 
