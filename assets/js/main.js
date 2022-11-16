927dc085133050b8e3d71c384788e5c023071865
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-16 14:30:22 +0900

    d
    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,32 @@ $(document).ready(function(){
     $(".mobile-gnb").toggleClass("on");
     return false
   })
+
+  var trailerswiper = new Swiper(".game-trailer .inner", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+    },
+  });
+
+  var fractionSwiper = new Swiper(".game-trailer .inner", {
+    pagination: {
+      el: ".swiper-pagination2",
+      type: "fraction",
+    },
+  });
+  trailerswiper.controller.control = fractionSwiper;
+
+
+
+
+
+
+
+
 })
+
