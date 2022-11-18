0002fca36d469057f3b44181869d7c646a39e044
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-18 14:03:30 +0900

    header hover event add
    mouseenter, mouseleave로
    애니메이션 구현

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-  $(".mobile-menu-btn a, .mobile-gnb .close-btn").on("click",function(){// 모바일 메뉴 버튼
+  $(".mobile-menu-btn a, .mobile-gnb .close-btn").on("click",function(){//모바일 메뉴 버튼 클릭
     $(".mobile-gnb").toggleClass("on");
     return false
   })
 
-  $(".gnb-wrap .gnb li, .gnb-down-menu").on('mouseenter', function(){
-    $(".gnb-down-menu").fadeIn(2000)
-    console.log("on")
+  $(".gnb li").on('mouseenter', function(){ // 헤더 마우스호버 이벤트 ON 
+    // $(".gnb-down-menu").css("height","300px");
+    $(".gnb-down-menu").animate({
+      height : "300px",
+    },300)
   })
-  $(".gnb-wrap .gnb li, .gnb-down-menu").on('mouseleave', function(){
-    $(".gnb-down-menu").fadeOut(2000)
-    console.log("off")
+  $("#header").on("mouseleave", function(){ // 헤더 마우스호버 이벤트 OFF
+    $(".gnb-down-menu").animate({
+      height : "0",
+    },300)
   })
 
-  var trailerswiper = new Swiper(".main-video-swiper", { //비디오 스와이퍼
+
+  var trailerswiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼
     loop: true,
     pagination: {
       el: ".swiper-pagination",
@@ -24,7 +28,7 @@ $(document).ready(function(){
     },
   });
 
-  var fractionSwiper = new Swiper(".main-video-swiper", { //비디오 스와이퍼(pagination 하나 더 추가)
+  var fractionSwiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼(pagination 하나 더 추가)
     pagination: {
       el: ".swiper-pagination2",
       type: "fraction",
