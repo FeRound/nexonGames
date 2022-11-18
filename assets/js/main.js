06826e6403e31768cc9f3e85cb1a8146373d8df2
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-18 14:25:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
       nextEl: ".swiper-button-next",
     },
   });
+  
   var fractionSwiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼(pagination 하나 더 추가)
     loop: true,
     pagination: {
