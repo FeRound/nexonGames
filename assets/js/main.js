acb01ec011a54c44f7101ad5f3bcc72471368b0c
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-18 14:23:46 +0900

    메인배너 수정
    progressBar 하단 넘버 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,8 +26,8 @@ $(document).ready(function(){
       nextEl: ".swiper-button-next",
     },
   });
-
   var fractionSwiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼(pagination 하나 더 추가)
+    loop: true,
     pagination: {
       el: ".swiper-pagination2",
       type: "fraction",
