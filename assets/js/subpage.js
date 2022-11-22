27b7a6bb7250034ea84560ed6bdc2b1da27b79a2
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-22 10:01:53 +0900

    v4 서브페이지
     작업중

---
+++ assets/js/subpage.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+  new daum.roughmap.Lander({ // 카카오 맵
+    "timestamp" : "1669011503007",
+    "key" : "2co27",
+  }).render();
+
+  new daum.roughmap.Lander({ // 카카오 맵
+    "timestamp" : "1669011787374",
+    "key" : "2co2j",
+  }).render();
+
+}
