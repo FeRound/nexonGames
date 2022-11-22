390baf2675334cb3add796f1500ce5e120f9b485
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-22 16:09:53 +0900

    v4 서브페이지
    90% 완성

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-  var swiper = new Swiper(".v4-swiper", {
+  var trailerswiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+    },
+  });
+  
+  var fractionSwiper = new Swiper(".main-video-swiper", { //메인배너 비디오 스와이퍼(pagination 하나 더 추가)
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination2",
+      type: "fraction",
+    },
+  });
+  trailerswiper.controller.control = fractionSwiper;
+
+  var setion2Swiper = new Swiper(".section2-swiper", { // 메인 section2 스와이퍼
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var charSwiper = new Swiper(".v4-swiper", { // V4 캐릭터 스와이퍼
     slidesPerView: 5,
     spaceBetween: 10,
     pagination: {
@@ -7,6 +35,16 @@ $(document).ready(function(){
       clickable: true,
     },
   })
+
+  var bottomSwiper = new Swiper(".bottom-swiper", { // game 하단 스와이퍼 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+  });
 })
 
 
